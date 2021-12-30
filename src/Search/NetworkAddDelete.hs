47b168f4a41fc1ed1d681fac2e8f75cd183e4b29
wardwheeler <wheeler@amnh.org>
2021-12-30 15:54:31 -0500

    Adding module for network edge manipulations

---
+++ src/Search/NetworkAddDelete.hs
@@ -0,0 +1,73 @@
+{- |
+Module      :  NetworkAddDelete.hs
+Description :  Module specifying graph egde adding and deleting functions
+Copyright   :  (c) 2021 Ward C. Wheeler, Division of Invertebrate Zoology, AMNH. All rights reserved.
+License     :
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project.
+
+Maintainer  :  Ward Wheeler <wheeler@amnh.org>
+Stability   :  unstable
+Portability :  portable (I hope)
+
+-}
+
+module Search.NetworkAddDelete  ( deleteNetEdge
+                                ) where
+
+import Types.Types
+import qualified ParallelUtilities       as PU
+import Control.Parallel.Strategies
+import qualified Utilities.LocalGraph    as LG
+import Debug.Trace
+import qualified GraphOptimization.Traversals as T
+import qualified GraphOptimization.PreOrderFunctions as PRE
+import qualified GraphOptimization.PostOrderFunctions as POS
+import qualified Data.List as L
+import GeneralUtilities
+
+-- | deleteEdge deletes an edge (checking if network) and rediagnoses graph
+-- naive for now
+deleteNetEdge :: GlobalSettings -> ProcessedData -> PhylogeneticGraph -> LG.Edge -> PhylogeneticGraph
+deleteNetEdge inGS inData inPhyloGraph edgeToDelete =
+   if LG.isEmpty $ thd6 inPhyloGraph then error "Empty input phylogenetic graph in deleteNetEdge"
+   else if not (LG.isNetworkEdge (fst6 inPhyloGraph) edgeToDelete) then error ("Edge to delete: " ++ (show edgeToDelete) ++ " not in graph:\n" ++ (LG.prettify $ fst6 inPhyloGraph))
+   else 
+       let delSimple = LG.delEdge edgeToDelete $ fst6 inPhyloGraph
+           leafGraph = LG.extractLeafGraph $ thd6 inPhyloGraph
+
+           -- prune other edges if now unused
+           pruneEdges = True
+
+           -- don't warn that edges are being pruned
+           warnPruneEdges = False
+
+           -- graph optimization from root
+           startVertex = Nothing
+
+           -- full two-pass optimization
+           newPhyloGraph = T.multiTraverseFullyLabelSoftWired inGS inData pruneEdges warnPruneEdges leafGraph startVertex delSimple
+       in
+       newPhyloGraph
