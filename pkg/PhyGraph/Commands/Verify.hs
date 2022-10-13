576155d2ca9120de67cecf2564329aaac619776e
wardwheeler <wheeler@amnh.org>
2022-10-13 13:23:20 -0400

    Updated docs with Thomspn info

--- pkg/PhyGraph/Commands/Verify.hs
+++ pkg/PhyGraph/Commands/Verify.hs
@@ -113,7 +113,7 @@ reportArgList = ["all", "data", "search", "graphs", "overwrite", "append", "dot"
 
 -- | search arguments
 searchArgList :: [String]
-searchArgList = ["days", "hours", "minutes", "seconds", "instances", "thompson", "mfactor", "simple", "linear", "exponential", "maxnetadges", "stop"]
+searchArgList = ["days", "hours", "minutes", "seconds", "instances", "thompson", "mfactor", "simple", "linear", "exponential", "maxnetedges", "stop"]
 
 -- | buildArgList is the list of valid build arguments
 selectArgList :: [String]
