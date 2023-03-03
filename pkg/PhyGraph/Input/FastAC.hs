60ed335c1ee28d29489df176a1cb6828550d37d3
wardwheeler <wheeler@amnh.org>
2023-03-03 10:30:10 -0500

    Refactored common elements between fast a and fastc char info parsing

--- pkg/PhyGraph/Input/FastAC.hs
+++ pkg/PhyGraph/Input/FastAC.hs
@@ -99,6 +99,7 @@ generateDefaultMatrix inAlph rowCount indelCost substitutionCost =
 -- | getFastaCharInfo get alphabet , names etc from processed fasta data
 -- this doesn't separate ambiguities from elements--processed later
 -- need to read in TCM or default
+-- only for single character element sequecnes
 getFastaCharInfo :: [TermData] -> String -> String -> Bool -> ([ST.ShortText], [[Int]], Double) -> CharInfo
 getFastaCharInfo inData dataName dataType isPrealigned localTCM =
     if null inData then error "Empty inData in getFastaCharInfo"
@@ -130,146 +131,28 @@ getFastaCharInfo inData dataName dataType isPrealigned localTCM =
                     AminoSeq -> toSymbols ["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y", "-"]
                     _        -> sequenceData
 
+            thisAlphabet =
+                case fst3 localTCM of
+                a | null a -> seqAlphabet
+                a          -> fromSymbols a
+
+
+        in
+        commonFastCharInfo dataName isPrealigned localTCM seqType thisAlphabet
+
+-- | commonFastCharInfo breaks out common functions between fasta and fastc parsing
+commonFastCharInfo :: String -> Bool -> ([ST.ShortText], [[Int]], Double) -> CharType -> Alphabet ST.ShortText-> CharInfo
+commonFastCharInfo dataName isPrealigned localTCM seqType thisAlphabet =
+
+        let
             localCostMatrix :: S.Matrix Int
             localCostMatrix = if null $ fst3 localTCM then
                                 let (indelCost, substitutionCost) = if null $ snd3 localTCM then (1,1)
                                                                     else ((head . head . snd3) localTCM,  (last . head . snd3) localTCM)
                                 in
-                                S.fromLists $ generateDefaultMatrix seqAlphabet 0 indelCost substitutionCost
+                                S.fromLists $ generateDefaultMatrix thisAlphabet 0 indelCost substitutionCost
                               else S.fromLists $ snd3 localTCM
 
-
-            (additionalWeight, localDenseCostMatrix, localWideTCM, localHugeTCM) = 
-                let dimension = fromIntegral $ V.length localCostMatrix
-
-                    -- this 2x2 so if some Show instances are called don't get error
-                    slimMetricNil = genDiscreteDenseOfDimension dimension                     
-                    wideMetricNil = metricRepresentation . snd $ TCM.fromRows [[0::Word, 0::Word],[0::Word, 0::Word]]
-                    hugeMetricNil = metricRepresentation . snd $ TCM.fromRows [[0::Word, 0::Word],[0::Word, 0::Word]]
-
-
-                    slimMetric = TCMD.generateDenseTransitionCostMatrix 0 dimension $ MR.retreiveSCM wideMetric
-                    (wideWeight, wideMetric) = getTCMMemo (thisAlphabet, localCostMatrix)
-                    (hugeWeight, hugeMetric) = getTCMMemo (thisAlphabet, localCostMatrix)
-
-                    resultSlim = (         1, slimMetric,    wideMetricNil, hugeMetricNil)
-                    resultWide = (wideWeight, slimMetricNil, wideMetric,    hugeMetricNil)
-                    resultHuge = (hugeWeight, slimMetricNil, wideMetricNil, hugeMetric)
-
-                in  case seqType of
-                      NucSeq   -> resultSlim
-                      SlimSeq  -> resultSlim
-                      WideSeq  -> resultWide
-                      AminoSeq -> resultWide
-                      HugeSeq  -> resultHuge
-                      _        -> error $ "getFastaCharInfo: Failure proceesing the CharType: '" <> show seqType <> "'"
-
-
-            tcmWeightFactor = thd3 localTCM
-
-            thisAlphabet =
-              case fst3 localTCM of
-                a | null a -> seqAlphabet
-                a          -> fromSymbols a
-
-            alignedSeqType = if not isPrealigned then seqType
-                             else
-                                if seqType `elem` [NucSeq, SlimSeq] then AlignedSlim
-                                else if seqType `elem` [WideSeq, AminoSeq] then AlignedWide
-                                else if seqType == HugeSeq then AlignedHuge
-                                else error "Unrecognozed data type in getFastaCharInfo"
-
-            defaultSeqCharInfo = emptyCharInfo {
-                                               charType = alignedSeqType
-                                             , activity = True
-                                             , weight = tcmWeightFactor * fromRational additionalWeight
-                                             , costMatrix = localCostMatrix
-                                             , slimTCM    = localDenseCostMatrix
-                                             , wideTCM    = localWideTCM
-                                             , hugeTCM    = localHugeTCM
-                                             , name       = T.pack (filter (/= ' ') dataName <> "#0")
-                                             , alphabet   = thisAlphabet
-                                             , prealigned = isPrealigned
-                                             , origInfo   = V.singleton (T.pack (filter (/= ' ') dataName <> "#0"), alignedSeqType, thisAlphabet)
-                                             }
-        in
-        --trace ("GFCI: " ++ (show localWideTCM) ++ "\n" ++ (show localHugeTCM)) (
-        -- trace ("FASTCINFO:" ++ (show $ charType defaultSeqCharInfo)) (
-        if (null . fst3) localTCM && (null . snd3) localTCM then 
-            trace ("Warning: no tcm file specified for use with fasta file : " ++ dataName ++ ". Using default, all 1 diagonal 0 cost matrix.") 
-            defaultSeqCharInfo
-        else 
-            trace ("Processing TCM data for file : "  ++ dataName) 
-            defaultSeqCharInfo
-        --)
-
--- | getTCMMemo creates the memoized tcm for large alphabet sequences
-getTCMMemo
-  :: ( FiniteBits b
-     , Hashable b
-     , NFData b
-     )
-  => (a, S.Matrix Int)
-  -> (Rational, MR.MetricRepresentation b)
-getTCMMemo (_inAlphabet, inMatrix) =
-    let (coefficient, tcm) = fmap transformGapLastToGapFirst . TCM.fromRows $ S.getFullVects inMatrix
-        metric = case tcmStructure $ TCM.diagnoseTcm tcm of
-                   NonAdditive -> discreteMetric
-                   Additive    -> linearNorm . toEnum $ TCM.size tcm
-                   _           -> metricRepresentation tcm
-    in (coefficient, metric)
-
-
--- | getSequenceAphabet take a list of ShortText with inform ation and accumulatiors
--- For both nonadditive and additve looks for [] to denote ambiguity and splits states
---  if splits on spaces if there are spaces (within []) (ala fastc or multicharacter states)
---  else if no spaces
---    if non-additive then each symbol is split out as an alphabet element -- as in TNT
---    if is additive splits on '-' to denote range
--- rescales (integerizes later) additive characters with decimal places to an integer type rep
--- for additive charcaters if states are not nummerical then throuws an error
-getSequenceAphabet :: [ST.ShortText]  -> [ST.ShortText] -> [ST.ShortText]
-getSequenceAphabet newAlph inStates =
-    if null inStates then
-        -- removes indel gap from alphabet if present and then (re) adds at end
-        -- (filter (/= (ST.singleton '-')) $ sort $ nub newAlph) ++ [ST.singleton '-']
-        L.sort (L.nub newAlph) ++ [ST.singleton '-']
-    else
-        let firstState = ST.toString $ head inStates
-        in
-        if head firstState /= '['  then
-            if firstState `elem` ["?","-"] then getSequenceAphabet  newAlph (tail inStates)
-            else getSequenceAphabet (head inStates : newAlph) (tail inStates)
-        else -- ambiguity
-            let newAmbigStates  = fmap ST.fromString $ words $ filter (`notElem` ['[',']']) firstState
-            in
-            getSequenceAphabet (newAmbigStates ++ newAlph) (tail inStates)
-
-
--- | getFastcCharInfo get alphabet , names etc from processed fasta data
--- this doesn't separate ambiguities from elements--processed later
--- need to read in TCM or default
-
---Not correct with default alphabet and matrix now after tcm recodeing added to loow for decmials I htink.
-getFastcCharInfo :: [TermData] -> String -> Bool -> ([ST.ShortText], [[Int]], Double) -> CharInfo
-getFastcCharInfo inData dataName isPrealigned localTCM =
-    if null inData then error "Empty inData in getFastcCharInfo"
-    else
-        --if null $ fst localTCM then errorWithoutStackTrace ("Must specify a tcm file with fastc data for fie : " ++ dataName)
-        let thisAlphabet = fromSymbols symbolsFound
-
-
-            symbolsFound
-              | not $ null $ fst3 localTCM = fst3 localTCM
-              | otherwise = getSequenceAphabet [] $ concatMap snd inData
-
-            localCostMatrix
-              | not $ null $ fst3 localTCM = S.fromLists $ snd3 localTCM
-              | otherwise = let (indelCost, substitutionCost) = if null $ snd3 localTCM then (1,1)
-                                                                else ((head . head . snd3) localTCM,  (last . head . snd3) localTCM)
-                            in
-                            S.fromLists $ generateDefaultMatrix thisAlphabet 0 indelCost substitutionCost
-
             tcmWeightFactor = thd3 localTCM
 
 
@@ -296,12 +179,6 @@ getFastcCharInfo inData dataName isPrealigned localTCM =
                       AminoSeq -> resultWide
                       HugeSeq  -> resultHuge
                       _        -> error $ "getFastaCharInfo: Failure proceesing the CharType: '" <> show seqType <> "'"
-
-            seqType =
-                case length thisAlphabet of
-                  n | n <=  8 -> SlimSeq
-                  n | n <= 64 -> WideSeq
-                  _           -> HugeSeq
 
             alignedSeqType = if not isPrealigned then seqType
                              else
@@ -327,14 +204,81 @@ getFastcCharInfo inData dataName isPrealigned localTCM =
         --trace ("GFCI: " <> (show localHugeTCM)) (
         --trace ("FCI " ++ (show $ length thisAlphabet) ++ " alpha size" ++ show thisAlphabet) (
         if (null . fst3) localTCM && (null . snd3) localTCM then 
-            trace ("Warning: no tcm file specified for use with fasta file : " ++ dataName ++ ". Using default, all 1 diagonal 0 cost matrix.") 
+            trace ("Warning: no tcm file specified for use with fasta/c file : " ++ dataName ++ ". Using default, all 1 diagonal 0 cost matrix.") 
             defaultSeqCharInfo
         else 
             trace ("Processing TCM data for file : "  ++ dataName) 
             defaultSeqCharInfo
-        -- )
 
--- | getSequenceAphabet takes a list of ShortText and returns the alp[habet and adds '-' if not present
+-- | getTCMMemo creates the memoized tcm for large alphabet sequences
+getTCMMemo
+  :: ( FiniteBits b
+     , Hashable b
+     , NFData b
+     )
+  => (a, S.Matrix Int)
+  -> (Rational, MR.MetricRepresentation b)
+getTCMMemo (_inAlphabet, inMatrix) =
+    let (coefficient, tcm) = fmap transformGapLastToGapFirst . TCM.fromRows $ S.getFullVects inMatrix
+        metric = case tcmStructure $ TCM.diagnoseTcm tcm of
+                   NonAdditive -> discreteMetric
+                   Additive    -> linearNorm . toEnum $ TCM.size tcm
+                   _           -> metricRepresentation tcm
+    in (coefficient, metric)
+
+
+-- | getSequenceAphabet take a list of ShortText with information and accumulatiors
+-- For both nonadditive and additve looks for [] to denote ambiguity and splits states
+--  if splits on spaces if there are spaces (within []) (ala fastc or multicharacter states)
+--  else if no spaces
+--    if non-additive then each symbol is split out as an alphabet element -- as in TNT
+--    if is additive splits on '-' to denote range
+-- rescales (integerizes later) additive characters with decimal places to an integer type rep
+-- for additive charcaters if states are not nummerical then throuws an error
+-- DOES not check for partitin characters
+getSequenceAphabet :: [ST.ShortText]  -> [ST.ShortText] -> [ST.ShortText]
+getSequenceAphabet newAlph inStates =
+    if null inStates then
+        -- removes indel gap from alphabet if present and then (re) adds at end
+        -- (filter (/= (ST.singleton '-')) $ sort $ nub newAlph) ++ [ST.singleton '-']
+        L.sort (L.nub newAlph) ++ [ST.singleton '-']
+    else
+        let firstState = ST.toString $ head inStates
+        in
+        if head firstState /= '['  then
+            if firstState `elem` ["?","-"] then getSequenceAphabet  newAlph (tail inStates)
+            else getSequenceAphabet (head inStates : newAlph) (tail inStates)
+        else -- ambiguity
+            let newAmbigStates  = fmap ST.fromString $ words $ filter (`notElem` ['[',']']) firstState
+            in
+            getSequenceAphabet (newAmbigStates ++ newAlph) (tail inStates)
+
+
+-- | getFastcCharInfo get alphabet , names etc from processed fasta data
+-- this doesn't separate ambiguities from elements--processed later
+-- need to read in TCM or default
+
+--Not correct with default alphabet and matrix now after tcm recodeing added to low for decmials I htink.
+-- only for multi-character element seqeunces 
+getFastcCharInfo :: [TermData] -> String -> Bool -> ([ST.ShortText], [[Int]], Double) -> CharInfo
+getFastcCharInfo inData dataName isPrealigned localTCM =
+    if null inData then error "Empty inData in getFastcCharInfo"
+    else
+        --if null $ fst localTCM then errorWithoutStackTrace ("Must specify a tcm file with fastc data for fie : " ++ dataName)
+        let symbolsFound
+              | not $ null $ fst3 localTCM = fst3 localTCM
+              | otherwise = getSequenceAphabet [] $ concatMap snd inData
+            
+            thisAlphabet = fromSymbols symbolsFound
+
+            seqType =
+                case length thisAlphabet of
+                  n | n <=  8 -> SlimSeq
+                  n | n <= 64 -> WideSeq
+                  _           -> HugeSeq
+
+        in
+        commonFastCharInfo dataName isPrealigned localTCM seqType thisAlphabet
 
 -- | getFastA processes fasta file
 -- assumes single character alphabet
@@ -375,7 +319,6 @@ getFastAText fileContents' fileName isPreligned =
             -- tail because initial split will an empty text
             if hasDupTerminals then errorWithoutStackTrace ("\tInput file " ++ fileName ++ " has duplicate terminals: " ++ show dupList)
             else pairData
-
 
 
 -- | getRawDataPairsFastA takes splits of Text and returns terminalName, Data pairs--minimal error checking
