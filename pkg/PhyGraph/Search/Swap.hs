aed7bbe15652ffe0d1c78e73a3ba37ac294a0665
wardwheeler <wheeler@amnh.org>
2023-03-10 10:35:51 -0500

    Changed endge union for rejoin DO to be IA-based.  Should be 1/2 time to check edge

--- pkg/PhyGraph/Search/Swap.hs
+++ pkg/PhyGraph/Search/Swap.hs
@@ -919,6 +919,7 @@ singleJoin swapType steepest inGS inData splitGraph splitGraphSimple splitCost d
    let newEdgeList = [(u, originalConnectionOfPruned, 0.0),(originalConnectionOfPruned, v, 0.0),(originalConnectionOfPruned, prunedGraphRootIndex, 0.0)]
 
        -- set edge union creation type to IA-based, filtering gaps
+       --targetEdgeData = M.makeEdgeData True True splitGraph charInfoVV targetEdge
        targetEdgeData = M.makeEdgeData doIA (not doIA) splitGraph charInfoVV targetEdge
 
        --this for SPR/NNI only
