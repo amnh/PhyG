47dbb74344364d6cf3c7cc36955a354e430a7b3a
Ward Wheeler <ward@wards-air.internal.amnh.org>
2023-06-09 16:16:29 -0400

    Updated OSX M1 binary

--- pkg/PhyGraph/phygraph.hs
+++ pkg/PhyGraph/phygraph.hs
@@ -91,7 +91,7 @@ main = do
     let splash = "\nPhyG version " ++ pgVersion ++ "\nCopyright(C) 2022-2023 Ward Wheeler and The American Museum of Natural History\n"
     let splash2 = "PhyG comes with ABSOLUTELY NO WARRANTY; This is free software, and may be \nredistributed "
     let splash3 = "\tunder the 3-Clause BSD License.\nCompiled " ++ compileDate
-    hPutStrLn stderr (splash ++ splash2 ++ splash3 ++ " Git commit: " ++ "2be51973dcfa797854c88a3edfaee95e5b9ec743") -- gitCommitHashString)
+    hPutStrLn stderr (splash ++ splash2 ++ splash3 ++ " Git commit: " ++ gitCommitHashString)
 
     -- Process arguments--a single file containing commands
     args <- getArgs
