cb3cd64251034b9f60b181c98d1de75dfcc23d3b
Recursion Ninja <github@recursion.ninja>
2023-07-18 14:36:59 -0400

    Adding templaate Haskell debug info

--- app/Software/License.hs
+++ app/Software/License.hs
@@ -14,7 +14,8 @@ import Data.Text.IO (readFile)
 import Instances.TH.Lift ()
 import Language.Haskell.TH hiding (Inline)
 import Language.Haskell.TH.Syntax hiding (Inline)
-import Paths_PhyG (getDataFileName)
+--import Paths_PhyG (getDataFileName)
+import Paths_PhyG
 import Prelude hiding (lines, readFile)
 import System.FilePath (normalise)
 
@@ -24,9 +25,28 @@ The text of the software license under which the PHANE tool is distributed.
 -}
 licenseText :: ExpQ
 licenseText =
+    let filePath = "doc/LICENSE"
+        gatherData = polishData . readFile . normalise
+        polishData = fmap (intercalate "\n" . lines)
+        inlineData = gatherData filePath >>= lift
+        noting key = putStrLn . ("\n>>> [TEMPLATE HASKELL NOTE]:\n\t" <>) . (key <>) . (":\t" <>)
+        workingIO = do
+            getDataFileName "LICENSE" >>= noting "getDataFileName"
+            getBinDir     >>= noting "getBinDir"
+            getLibDir     >>= noting "getLibDir"
+            getDynLibDir  >>= noting "getDynLibDir"
+            getDataDir    >>= noting "getDataDir"
+            getLibexecDir >>= noting "getLibexecDir"
+            getSysconfDir >>= noting "getSysconfDir"
+            inlineData
+    in  addDependentFile filePath *> runIO workingIO
+{-
+licenseText :: ExpQ
+licenseText =
     let locateData = runIO $ getDataFileName "LICENSE"
         gatherData = polishData . readFile . normalise
         polishData = fmap (intercalate "\n" . lines)
     in  do  filePath <- locateData
             addDependentFile filePath
             runIO $ gatherData filePath >>= lift 
+-}
