f8d10d7d6b05b4c70c99b70e99eed99c788ecd89
Recursion Ninja <github@recursion.ninja>
2023-07-19 08:26:47 -0400

    TH debugging [1]

--- app/Software/License.hs
+++ app/Software/License.hs
@@ -9,6 +9,8 @@ module Software.License
   ( licenseText
   ) where
 
+import Control.Monad (filterM)
+import Data.Foldable
 import Data.Text (intercalate, lines)
 import Data.Text.IO (readFile)
 import Instances.TH.Lift ()
@@ -17,7 +19,7 @@ import Language.Haskell.TH.Syntax hiding (Inline)
 --import Paths_PhyG (getDataFileName)
 import Paths_PhyG
 import Prelude hiding (lines, readFile)
-import System.FilePath (normalise)
+import System.FilePath ((</>), normalise)
 import System.Directory
 
 
@@ -31,6 +33,8 @@ licenseText =
         polishData = fmap (intercalate "\n" . lines)
         inlineData = gatherData filePath >>= lift
         noting key = putStrLn . ("\n>>> [TEMPLATE HASKELL NOTE]:\n\t" <>) . (key <>) . (":\t" <>)
+        findLicense = getDirFiltered (const (pure True))  -- (pure . (== "LICENSE"))
+        
         workingIO = do
             getDataFileName "LICENSE" >>= noting "getDataFileName"
             getBinDir     >>= noting "getBinDir"
@@ -39,7 +43,11 @@ licenseText =
             getDataDir    >>= noting "getDataDir"
             getLibexecDir >>= noting "getLibexecDir"
             getSysconfDir >>= noting "getSysconfDir"
-            getCurrentDirectory >>= noting "CurrentDir"
+            cwd <- getCurrentDirectory
+            noting "CurrentDir" cwd
+            listDirectory cwd >>= noting "listDirectory" . show
+            found <- findLicense cwd
+            noting "findLicense" $ show found
             inlineData
     in  addDependentFile filePath *>
             (location >>= runIO . noting "location" . show)
@@ -54,3 +62,33 @@ licenseText =
             addDependentFile filePath
             runIO $ gatherData filePath >>= lift 
 -}
+
+
+{-# INLINE getDirFiltered #-}
+-- | Recursively get all files and subdirectories in the given directory that
+-- satisfy the given predicate. Note that the content of subdirectories not
+-- matching the filter is ignored. In particular, that means something like
+-- @getDirFiltered doesFileExist@ will /not/ recursively return all files.
+--
+-- @since 0.2.2.0
+getDirFiltered :: (FilePath -> IO Bool) -- ^ Filepath filter
+               -> FilePath
+               -> IO [FilePath]
+getDirFiltered check path =
+    let mkRel = (path </>)
+        foldMapA = (fmap fold .) . traverse
+        canDecend fp = do
+            isDir <- doesDirectoryExist fp
+            isDig <- searchable <$> getPermissions fp
+            pure $ isDir && isDig
+
+    in  do  all' <- listDirectory path
+            curr <- filterM check (mkRel <$> all')
+            dirs <- filterM canDecend all'
+            case dirs of
+                [] -> pure curr
+                ds -> do
+                    next <- foldMapA (getDirFiltered check) ds
+                    pure $ curr <> next
+
+
