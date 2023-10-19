a330afa466ee43bbc0bfe53bd49befc6e1d92a37
Recursion Ninja <github@recursion.ninja>
2023-10-19 11:05:48 -0400

    Fixing with unliftIO

--- src/Search/Search.hs
+++ src/Search/Search.hs
@@ -1,36 +1,5 @@
 {- |
-Module      :  Search.hs
-Description :  Module controlling timed randomized search functions
-Copyright   :  (c) 2022 Ward C. Wheeler, Division of Invertebrate Zoology, AMNH. All rights reserved.
-License     :
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-The views and conclusions contained in the software and documentation are those
-of the authors and should not be interpreted as representing official policies,
-either expressed or implied, of the FreeBSD Project.
-
-Maintainer  :  Ward Wheeler <wheeler@amnh.org>
-Stability   :  unstable
-Portability :  portable (I hope)
+Module controlling timed randomized search functions
 -}
 module Search.Search (
     search,
@@ -44,6 +13,7 @@ import Control.Evaluation
 import Control.Exception
 import Control.Monad (join, when)
 import Control.Monad.IO.Class (MonadIO (..))
+import Control.Monad.IO.Unlift
 import Control.Monad.Logger (LogLevel (..), Logger (..), Verbosity (..))
 import Data.Bifunctor (bimap)
 import Data.Char
@@ -66,6 +36,7 @@ import System.Timeout
 import System.Timing
 import Text.Read
 import Types.Types
+import UnliftIO.Async (pooledMapConcurrently)
 import Utilities.LocalGraph qualified as LG
 import Utilities.Utilities qualified as U
 
@@ -183,7 +154,7 @@ search inArgs inGS inData pairwiseDistances rSeed inGraphList' =
                 let threadCount = instances -- <- (max 1) <$> getNumCapabilities
                 let startGraphs = replicate threadCount (inGraphList, mempty)
                 let threadInits = zip3 infoIndices seadStreams startGraphs
-                resultList ← liftIO $ mapConcurrently (liftIO . searchTimed) threadInits -- If there are no input graphs--make some via distance
+                resultList ← pooledMapConcurrently searchTimed threadInits -- If there are no input graphs--make some via distance
                 pure $
                     let (newGraphList, commentList) = unzip resultList
                         newCostList = L.group $ L.sort $ fmap getMinGraphListCost newGraphList
